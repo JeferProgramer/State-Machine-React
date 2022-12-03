b0637b50bccf969353f17d1599920d12b1cc5055
JeferProgramer <jefer.talento.11@gmail.com>
2022-12-02 19:30:33 -0500

    base layout

---
+++ src/Machines/bookingMachine.js
@@ -0,0 +1,43 @@
+import { createMachine } from "xstate";
+
+const bookingMachine = createMachine({
+  id: "buy plane tickets",
+  initial: "initial",
+  states: {
+    initial: {
+      on: {
+        START: {
+          target: "search",
+          actions: 'imprimirInicio'
+        }
+      },
+    },
+    search: {
+      entry: 'imprimirEntrada',
+      exit: 'imprimirSalida',
+      on: {
+        CONTINUE: "passengers",
+        CANCEL: "initial",
+      },
+    },
+    tickets: {
+      on: {
+        FINISH: "initial",
+      },
+    },
+    passengers: {
+      on: {
+        DONE: "tickets",
+        CANCEL: "initial",
+      },
+    },
+  },
+},{
+  actions: {
+    imprimirInicio: () => console.log('Imprimir inicio'),
+    imprimirEntrada: () => console.log('Imprimir entrada a search'),
+    imprimirSalida: () => console.log('Imprimir salida del search'),
+  }
+});
+
+export default bookingMachine;
